d11a377e889e630536075c0f60b92452392d0918
John Smart <john.smart@wakaleo.com>
2013-10-09 18:10:08 +1100

    added specflow sample code

---
+++ flying-high-specflow/FlyingHighSpecs/EarningExtraPointsFromFrequentFlyerStatusSteps.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace FlyingHighSpecs
+{
+    [Binding]
+    public class EarningExtraPointsFromFrequentFlyerStatusSteps
+    {
+        [When(@"I fly on a flight that is worth (.*) base points")]
+        public void WhenIFlyOnAFlightThatIsWorthBasePoints(int p0)
+        {
+         //   ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should earn a status bonus of (.*)")]
+        public void ThenIShouldEarnAStatusBonusOf(int p0)
+        {
+           // ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should have guaranteed minimum earned points per trip of (.*)")]
+        public void ThenIShouldHaveGuaranteedMinimumEarnedPointsPerTripOf(int p0)
+        {
+            //ScenarioContext.Current.Pending();
+        }
+
+        public void foo(Table data) 
+        {
+               
+        }
+        
+        [Then(@"I should earn (.*) points in all")]
+        public void ThenIShouldEarnPointsInAll(int p0)
+        {
+            //ScenarioContext.Current.Pending();
+        }
+    }
+}
---
+++ flying-high-specflow/FlyingHighSpecs/EarningFrequentFlyerPointsFromFlightsSteps.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System;
+using TechTalk.SpecFlow;
+
+namespace FlyingHighSpecs
+{
+    [Binding]
+    public class EarningFrequentFlyerPointsFromFlightsSteps
+    {
+
+
+        /*
+         *     Given I am a <status> Frequent Flyer member
+    When I fly on a flight that is worth <base> base points
+    Then I should earn a status bonus of <bonus>
+    And I should have guaranteed minimum earned points per trip of <minimum>
+    And I should earn <total> points in all
+         */
+        [Given(@"the flying distance between (.*) and (.*) is (.*) km")]
+        public void GivenTheFlyingDistanceBetweenSydneyAndMelbourneIsKm(string departure,
+                                                                        string destination,
+                                                                        int distance)
+        {
+           // TODO
+        }
+        
+
+        [Given(@"I am a (.*) Frequent Flyer member")]
+        public void GivenIAmAFrequentFlyerMemberOfStatus(string status)
+        {
+        }
+
+        /*
+        [Given]
+        public void Given_I_am_a_STATUS_Frequent_Flyer_member(string status)
+        {
+            // TODO
+        }
+        */
+        
+        [When(@"I fly from (.*) to (.*)")]
+        public void WhenIFlyBetween(string departure, string destination)
+        {
+        }
+        
+        [Then(@"I should earn (.*) points")]
+        public void ThenIShouldEarnPoints(int points)
+        {
+        }
+    }
+}
